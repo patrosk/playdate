10c1654569f2e3936e037b826793cfb92fe3d2df
patrosk <nema@itiden.se>
2023-05-26 09:06:34 +0200

    add favicon and create function to make trash items fall down from top

--- src/app.js
+++ src/app.js
@@ -53,43 +53,14 @@ const textStyle = new PIXI.TextStyle({
 const text = new PIXI.Text('', textStyle);
 
 // Kim
-const kim1 = new PIXI.Sprite.from('/src/resources/kim_1.svg');
-kim1.height = 100;
-kim1.width = 100;
-kim1.y = 525;
-kim1.x = 450;
-
-// Trash
-// const trash1 = new PIXI.Sprite.from('/src/resources/trash_1.svg');
-// trash1.height = 40;
-// trash1.width = 30;
-// const trash2 = new PIXI.Sprite.from('/src/resources/trash_2.svg');
-// trash2.height = 40;
-// trash2.width = 30;
-// const trash3 = new PIXI.Sprite.from('/src/resources/trash_3.svg');
-// trash3.height = 40;
-// trash3.width = 30;
-//const trash4 = new PIXI.Sprite.from('/src/resources/trash_4.svg');
-// trash4.height = 40;
-// trash4.width = 30;
+const kim = new PIXI.Sprite.from('/src/resources/kim_1.svg');
+kim.height = 100;
+kim.width = 100;
+kim.y = 525;
+kim.x = 450;
 
 stage.addChild(text);
-stage.addChild(kim1);
-//stage.addChild(trash1);
-
-// Tweak this function later!
-// function update() {
-//   trash1.position.y -= 40;
-
-//   if (kim1.score > 8) {
-//     trash1.position.y -= 6;
-//   } else if (kim1.score > 25) {
-//     trash1.position.y -= 7;
-//   } else if (kim1.score > 40) {
-//     trash1.position.y -= 8;
-//   }
-//   requestAnimationFrame(update);
-// }
+stage.addChild(kim);
 
 let playerScore;
 
@@ -100,9 +71,9 @@ function score() {
     fill: ['#ff8642'],
   });
 
-  kim1.score = -1;
+  kim.score = -1;
 
-  playerScore = new PIXI.Text(kim1.score, style);
+  playerScore = new PIXI.Text(kim.score, style);
 
   stage.addChild(playerScore);
 
@@ -125,38 +96,36 @@ function animate() {
 
 animate();
 
-// function checkIfCollide() {
-//   if (
-//     trash1.position.x > kim1.position.x - boxWidth / 2 &&
-//     trash1.position.x < kim1.position.x + boxWidth / 2 &&
-//     trash1.position.y === kim1.position.y
-//   ) {
-//     goalBoxSpawn();
-//     kim1.score++;
-//     playerScore.text = kim1.score;
-//   } else if (trash1.position.x === 0) {
-//     //gameOverScreen.visible = true;
-//     //stage.visible = false;
-//     //startPage.visible = false;
-//   }
-// }
-
-
 // Function to move Kim horizontally
 document.addEventListener('keydown', onKeyDown);
 function onKeyDown(key) {
   if (key.keyCode === 37) {
-    if (kim1.position.x != 0) {
-      kim1.position.x -= boxWidth;
+    if (kim.position.x != 0) {
+      kim.position.x -= boxWidth;
     }
   }
 
   if (key.keyCode === 39) {
-    if (kim1.position.x != app.screen.width - (boxWidth + 50)) {
-      kim1.position.x += boxWidth;
+    if (kim.position.x != app.screen.width - (boxWidth + 50)) {
+      kim.position.x += boxWidth;
     }
   }
 }
+
+
+// Tweak this function later!
+// function update() {
+//   trash1.position.y -= 40;
+
+//   if (kim.score > 8) {
+//     trash1.position.y -= 6;
+//   } else if (kim.score > 25) {
+//     trash1.position.y -= 7;
+//   } else if (kim.score > 40) {
+//     trash1.position.y -= 8;
+//   }
+//   requestAnimationFrame(update);
+// }
 
 // Interval to spawn new trash items
 let trashItems = [];
@@ -164,6 +133,7 @@ function generateTrashItems() {
   setInterval(() => {
     const newTrashItem = createTrashItem();
     trashItems.push(newTrashItem);
+    fallDown(newTrashItem);
   }, 1000);
 }
 
@@ -178,6 +148,30 @@ function createTrashItem() {
   trashItem.position.y = 0;
 
   stage.addChild(trashItem);
-
   return trashItem;
+}
+
+// Function to make trash items move vertically
+function fallDown(trashItem) {
+  setInterval(() => {
+    trashItem.position.y += 10;
+    checkIfCollide(trashItem);
+  }, 125);
+}
+
+// Function to check if Kim and trash items collide
+ function checkIfCollide(trashItem) {
+  console.log(trashItem.position.y);
+  if (
+    // trashItem.position.x > kim.position.x - boxWidth / 2 &&
+    // trashItem.position.x < kim.position.x + boxWidth / 2 &&
+    trashItem.position.y >= 585
+  ) {
+    kim.score++;
+    playerScore.text = kim.score;
+  }
+    //gameOverScreen.visible = true;
+    //stage.visible = false;
+    //startPage.visible = false;
+
 }
